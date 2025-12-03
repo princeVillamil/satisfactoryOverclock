015a09e5072f88b40392bf7edb832f99e8d3008e
princeVillamil <jeffreyvillamilwork@gmail.com>
2025-12-03 15:28:13 +0800

    day2

--- client/app/routes.ts
+++ client/app/routes.ts
@@ -1,8 +1,13 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type 
+        RouteConfig, 
+        index, 
+        route 
+    } from "@react-router/dev/routes";
 
 export default [
     index("routes/home.tsx"),
-    index("routes/satisfactorycalculator.tsx")
+    route("satisfactorycalculator","routes/satisfactorycalculator.tsx"),
+    route("teams/:teamId", "routes/test.tsx"),
 
 ]satisfies RouteConfig;
 // export default [index("routes/satisfactory-calculator.tsx")] satisfies RouteConfig;
---
+++ client/app/routes/test.tsx
@@ -0,0 +1,18 @@
+import type { Route } from "./+types/test";
+
+export default function MyRouteComponent({
+  loaderData,
+  actionData,
+  params,
+  matches,
+}: Route.ComponentProps) {
+  return (
+    <div>
+      <h1>Welcome to My Route with Props!</h1>
+      <p>Loader Data: {JSON.stringify(loaderData)}</p>
+      <p>Action Data: {JSON.stringify(actionData)}</p>
+      <p>Route Parameters: {JSON.stringify(params)}</p>
+      <p>Matched Routes: {JSON.stringify(matches)}</p>
+    </div>
+  );
+}
